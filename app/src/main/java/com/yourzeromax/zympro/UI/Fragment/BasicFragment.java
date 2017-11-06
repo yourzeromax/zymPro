933ce6cf14fe441bbccd748b67c75eca4900e522
yourzeromax <yourzeromax@163.com>
2017-11-06 19:32:58 +0800

    修改percentLayout前
    1

--- app/src/main/java/com/yourzeromax/zympro/UI/Fragment/BasicFragment.java
+++ app/src/main/java/com/yourzeromax/zympro/UI/Fragment/BasicFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yourzeromax.zympro.R;
 
@@ -14,6 +16,7 @@ import com.yourzeromax.zympro.R;
  */
 
 public class BasicFragment extends Fragment {
+    TextView tvshow;
     public BasicFragment() {
     }
 
@@ -21,6 +24,13 @@ public class BasicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_basic,null);
+        tvshow = (TextView)view.findViewById(R.id.choose_speciality);
+        tvshow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(), "123", Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
 
