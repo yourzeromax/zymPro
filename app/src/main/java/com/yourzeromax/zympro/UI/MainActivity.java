82b211ca4c48606f2086097587d51e46ec5d5f33
yourzeromax <yourzeromax@163.com>
2017-11-06 09:42:03 +0800

    修改前的备份
    mainActivity

--- app/src/main/java/com/yourzeromax/zympro/UI/MainActivity.java
+++ app/src/main/java/com/yourzeromax/zympro/UI/MainActivity.java
@@ -1,47 +1,107 @@
 package com.yourzeromax.zympro.UI;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import com.yourzeromax.zympro.Application.C;
 import com.yourzeromax.zympro.R;
-import com.yourzeromax.zympro.Utils.OkHttp3Utils;
-
-import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
     @Bind(R.id.tb_main)
     android.support.v7.widget.Toolbar mToolbar;
+    @Bind(R.id.basic_user_icon)
+    CircleImageView mUserIcon;
+    @Bind(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @Bind(R.id.nav_view)
+    NavigationView mNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        viewInit();
+    }
+
+    private void viewInit() {
         toolbarInit();
+        listenerInit();
     }
 
-    private void toolbarInit(){
+    private void toolbarInit() {
         setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        try {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
+    private void listenerInit() {
+        mUserIcon.setOnClickListener(this);
+        mNavigationView.setNavigationItemSelectedListener(this);
+    }
+
+    private void openDrawer() {
+        if (!mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mainitems,menu);
+        getMenuInflater().inflate(R.menu.mainitems, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.basic_user_icon:
+                openDrawer();
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Toast.makeText(this, "about", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_icon:
+                Toast.makeText(this, "icon", Toast.LENGTH_SHORT).show();
+        }
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
