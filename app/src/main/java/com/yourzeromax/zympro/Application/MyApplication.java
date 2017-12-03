fa7267a12269d76d0374c6cd928ebb8bddc7946a
yourzeromax <yourzeromax@163.com>
2017-12-03 10:17:50 +0800

    1
    1

--- app/src/main/java/com/yourzeromax/zympro/Application/MyApplication.java
+++ app/src/main/java/com/yourzeromax/zympro/Application/MyApplication.java
@@ -18,10 +18,37 @@ import java.util.List;
  * Created by yourzeromax on 2017/11/3.
  */
 
-public class MyApplication extends LitePalApplication {
-    @Override
-    public void onCreate() {
-        super.onCreate();
+public class MyApplication extends MobApplication {
+
+
+    /**
+     * Global application context.
+     */
+    static Context sContext;
+
+    /**
+     * Construct of LitePalApplication. Initialize application context.
+     */
+    public MyApplication() {
+        sContext = this;
     }
 
+
+    @Deprecated
+    public static void initialize(Context context) {
+        sContext = context;
+    }
+
+    /**
+     * Get the global application context.
+     *
+     * @return Application context.
+     * @throws org.litepal.exceptions.GlobalException
+     */
+    public static Context getContext() {
+        if (sContext == null) {
+            throw new GlobalException(GlobalException.APPLICATION_CONTEXT_IS_NULL);
+        }
+        return sContext;
+    }
 }
--- app/src/main/java/com/yourzeromax/zympro/UI/RegisterActivity.java
+++ app/src/main/java/com/yourzeromax/zympro/UI/RegisterActivity.java
@@ -5,23 +5,57 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.yourzeromax.zympro.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 
 public class RegisterActivity extends BaseActivity implements View.OnClickListener {
     @Bind(R.id.btn_zc)
     Button btnRegister;
+    EventHandler eventHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // SMSSDK.setAskPermisionOnReadContact(boolShowInDialog);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
         ButterKnife.bind(this);
         btnRegister.setOnClickListener(this);
+
+        eventHandler = new EventHandler() {
+            public void afterEvent(int event, int result, Object data) {
+                if (data instanceof Throwable) {
+                    Throwable throwable = (Throwable) data;
+                    String msg = throwable.getMessage();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(RegisterActivity.this, "123", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {
+                        //处理自己的逻辑
+                    }
+                }
+            }
+        };
+
+        // 注册监听器
+        SMSSDK.registerEventHandler(eventHandler);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SMSSDK.unregisterEventHandler(eventHandler);
     }
 
     @Override
